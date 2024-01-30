37b0dd1dcbc900b7b63e39b8082aab33272d9e42
Alexandr S <allexandr.s@gmail.com>
2024-01-30 13:18:25 +0300

    leson1:Task3

---
+++ lesson1/Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 6;
+int b = 5;
+int c = 2;
+int d = 13;
+int dom1 = a * b;
+Console.WriteLine(dom1);
+
+int dom2 = c + d;
+Console.WriteLine(dom2);
+
+int dom3 = dom1 / dom2;
+Console.WriteLine(dom3);
+
