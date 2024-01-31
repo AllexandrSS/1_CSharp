f214edb8448440a0cb6e51e58621d3d0ae90c820
Alexandr S <allexandr.s@gmail.com>
2024-01-31 23:35:13 +0300

    leson2:Task4

---
+++ lesson2/Task4/Program.cs
@@ -0,0 +1,36 @@
+﻿int n = 5;
+int[] array = { 2, 4, 7, 3, 9 };
+int i = 0;
+int max = array[0];
+
+while (i < n)
+{
+    if(array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+Console.WriteLine(max);
+
+//Решение той же задачи с помощью for
+max = array[0];
+for (int j = 0; j < n; j++)
+{
+    if (array[j] > max)
+    {
+        max = array[j];
+    }
+}
+Console.WriteLine(max);
+
+// Решение той же задачи с помощью foreach
+max = array[0];
+foreach(int e in array)
+{
+    if (e > max)
+    {
+        max = e;
+    }
+}
+Console.WriteLine(max);
